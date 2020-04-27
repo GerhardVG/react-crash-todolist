d157d0ca836e1a6f610c8343a0f84ba7c524b642
Gerhard vanGraan <Gerhard.vanGraan@partner.bmw.co.za>
2020-04-27 09:43:59 +0200

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is the TodoList app v1.0.0 built ith React.</p>
+        </div>
+    )
+}
+
+export default About;
+
